f7dad8d2b1075e117f1c26e58e762406c70cc7ae
Louis de Wardt <contact@louis.dewardt.uk>
2020-09-26 15:40:56 +0100

    Added example code

---
+++ joker.py
@@ -0,0 +1,19 @@
+jokes = [
+  {
+    'question': 'Why didn\'t the mummy have any friends?',
+    'punchline': 'Because he was wrapped up in himself!'
+  },
+  {
+    'question': 'What road has the most ghosts haunting it?',
+    'punchline': 'A dead end'
+  }
+]
+
+
+
+for joke in jokes:
+  print('Q: ' + joke['question'])
+  input()
+  print('A: ' + joke['punchline'])
+  input()
+  print('\n')
